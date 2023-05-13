09f2dcf025a963c4986bd59a2e1e177e6b4e8cee
Carwyn Collinsworth <carwyn.collinsworth@stonybrook.edu>
2023-05-13 02:06:38 -0400

    not learning but model and structure is there. need to add test train split.

---
+++ model.py
@@ -0,0 +1,63 @@
+"""
+Big Data Analytics
+Team BANC
+Team Members:   Balaji Jayasankar, Aniket Malsane, Nishit Jain, and Carwyn Collinsworth
+Associated IDs: 114360535,         115224188,      112680897,       112605735
+
+This code is sampled from Assignment 3 - and by extension,
+https://pytorch.org/tutorials/beginner/transformer_tutorial.html 
+"""
+
+import math
+import numpy as np
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
+class TransformerModel(nn.Module):
+    
+    def __init__(self, d_model: int, nhead: int, d_hid: int,
+                 nlayers: int, dropout: float = 0.5):
+        """This method constructs a torch.nn.Module type superclass which is a model for implementing a transformer model for a language modeling task. This is called when a new object of type TransformerModel is instantiated. 
+
+        Args:
+            d_model: the expected number of inputs to the TransformerEncoderLayer and PositionalEncoding
+            nhead: the number of heads in the multiheadattention model
+            d_hid: the hidden dimension of the feedforward network in the TransformerEncoderLayer
+            nlayers: number of sub-encoder-layers in the encoder (TransformerEncoder)
+            dropout: probability of dropout (setting output to zero) in both the positional encoding and the transformer encoder layer.
+
+        Returns:
+            a TransformerModel object, which is an extension of the base nn.Module class pytorch class - an object capable of building NNs from which keeps track of trainable parameters, etc.
+        """
+        # Instantiate the superclass (nn.Module) such that our model can build upon it.
+        super().__init__()
+        # Define a variable that specifies the type of module. This is never accessed and is not necessary.
+        self.model_type = 'Transformer'
+        # Define a neural network component to contain a single self attention and respective feed forward layer (one TransformerEncoderLayer).
+        # Added norm_first=True to add batch normalization
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout, norm_first=True)
+        # Define a consectutive sequence of encoder_layers layers of transformer encoder layers, using our previous definition.
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        # Define a decoder as a linear layer to take the output of the attention component and convert it to a specified number of tokens.
+        self.decoder = nn.Linear(d_model, 11)
+        # Define the expected input size as an attribute of the model for later references.
+        self.d_model = d_model
+
+    def forward(self, src: Tensor) -> Tensor:
+        """
+        Args:
+            src: Tensor, shape [seq_len, batch_size]
+            src_mask: Tensor, shape [seq_len, seq_len]
+
+        Returns:
+            output Tensor of shape [seq_len, batch_size, ntoken]
+        """
+        output = self.transformer_encoder(src)
+        output = self.decoder(output)
+        return output
+    
+def generate_square_subsequent_mask(sz: int) -> Tensor:
+    """Generates an upper-triangular matrix of -inf, with zeros on diag."""
+    return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1)
--- train.py
+++ train.py
@@ -11,11 +11,15 @@ Sample command to run:
 $ python3 train.py
 ```
 """
-
+import time
+import math
 import torch
 import numpy as np
 from tqdm import tqdm
+from torch import nn
 from torch.utils.data import Dataset, DataLoader
+
+from model import TransformerModel
 
 def load_dataset(embed_dataset_name, labeled_dataset_name, label_map):
     #BEGIN[ChatGPT]
@@ -36,9 +40,28 @@ def load_dataset(embed_dataset_name, labeled_dataset_name, label_map):
     return dataloader, labelloader
 
 def setup_model():
-    pass
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train(dataloader, labelloader):
+    emsize = 768  # embedding dimension
+    d_hid = 250  # dimension of the feedforward network model in ``nn.TransformerEncoder``
+    nlayers = 3  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
+    nhead = 4  # number of heads in ``nn.MultiheadAttention``
+    dropout = 0.2  # dropout probability
+    model = TransformerModel(emsize, nhead, d_hid, nlayers, dropout).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    lr = 0.001  # learning rate
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    return model, criterion, optimizer, device
+
+def train(dataloader, labelloader, model, criterion, optimizer, device):
+
+    # Setup training
+    model.train()
+    log_interval = 200
+    total_loss = 0.
 
     # Ensure that the labels and data have the same size
     num_batches = len(dataloader)
@@ -51,6 +74,25 @@ def train(dataloader, labelloader):
     for idx, batch in tqdm(enumerate(dataloader), total=num_batches):
         labels = next(label_loader_enum)
 
+        batch = batch.to(device)
+        labels = labels.to(device)
+
+        output = model(batch)
+
+        loss = criterion(output, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        if idx % log_interval == 0 and idx > 0:
+            cur_loss = total_loss / log_interval
+            ppl = math.exp(cur_loss)
+            # print(f'| epoch {idx:3d} | {idx:5d}/{num_batches:5d} batches | '
+            #       f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            print(output)
+            total_loss = 0
 
 #BEGIN[ChatGPT]"I have data in a nonstandard format - a string that represents an array for each record. The data is too big to fit in memory. How do I use the pytorch dataloader to efficiently train on this data"
 class RedditDataset(Dataset):
@@ -62,7 +104,7 @@ class RedditDataset(Dataset):
     
     def __getitem__(self, index):
         #END[ChatGPT]
-        return torch.tensor(np.fromstring(self.data[index].strip()[1:-1], sep=' '))
+        return torch.tensor(np.fromstring(self.data[index].strip()[1:-1], sep=' ').astype(np.float32))
 #END[ChatGPT]
 
 class RedditLabels(Dataset):
@@ -74,7 +116,10 @@ class RedditLabels(Dataset):
         return len(self.data)
     
     def __getitem__(self, index):
-        return torch.tensor(int(self.label_map[self.data[index].strip().split(',')[0]]))
+        num_classes = 11
+        val = int(self.label_map[self.data[index].strip().split(',')[0]])
+        one_hot_matrix = np.eye(num_classes)[val]
+        return torch.tensor(one_hot_matrix)
 
 if __name__ == "__main__":
     embed_dataset_name = 'filtered_data/embed_data.txt'
@@ -96,4 +141,6 @@ if __name__ == "__main__":
 
     dataloader, labelloader = load_dataset(embed_dataset_name, labeled_dataset_name, label_map)
 
-    train(dataloader, labelloader)
+    model, criterion, optimizer, device = setup_model()
+
+    train(dataloader, labelloader, model, criterion, optimizer, device)
